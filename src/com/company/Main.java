0bf6290f90d34d6b74f94323fba6e5494f7cb0ef
BhavinThakkar18 <bhavin.majethia18@gmail.com>
2022-01-11 19:27:59 +0530

    Initial Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int myValue = 10000;
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue= Integer.MAX_VALUE;
+        System.out.println("MIN_INT Value: "+myMinIntValue);
+        System.out.println("MAX_INT Value: "+myMaxIntValue);
+    }
+}
