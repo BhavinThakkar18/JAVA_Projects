0d1f548c23d5a49f7581d6ca835da111e0d3efca
BhavinThakkar18 <bhavin.majethia18@gmail.com>
2022-01-11 19:34:12 +0530

    Underflow and Overflow

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,5 +8,7 @@ public class Main {
         int myMaxIntValue= Integer.MAX_VALUE;
         System.out.println("MIN_INT Value: "+myMinIntValue);
         System.out.println("MAX_INT Value: "+myMaxIntValue);
+        System.out.println("Busted MAX Value "+ (myMaxIntValue + 1)); // Overflow
+        System.out.println("Busted MIN Value "+ (myMinIntValue - 1)); // Underflow
     }
 }
