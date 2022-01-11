60a24feebcea588ea22760943b14c2280e494ed8
BhavinThakkar18 <bhavin.majethia18@gmail.com>
2022-01-11 20:04:01 +0530

    Min & MAX vaules for Byte and Short Data Types

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,5 +10,17 @@ public class Main {
         System.out.println("MAX_INT Value: "+myMaxIntValue);
         System.out.println("Busted MAX Value "+ (myMaxIntValue + 1)); // Overflow
         System.out.println("Busted MIN Value "+ (myMinIntValue - 1)); // Underflow
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+
+        System.out.println("Min_Byte Value: "+ myMinByteValue);
+        System.out.println("Max_Byte Value: "+ myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+
+        System.out.println("Min Short value "+ myMinShortValue);
+        System.out.println("Max Short Value:" + myMaxShortValue);
     }
 }
